93eadbe34373a983fc4199971cfbc56e2918237a
FNorton <fn@intelligentenvironments.com>
2018-12-21 12:47:46 +0000

    initial commit

---
+++ pythonisasnake.py
@@ -0,0 +1,11 @@
+#- dev Johnny Norton
+
+
+name = input  ("What is your name?:")
+lname = input ("What is your last name?:")
+age = input ("What is your age?:")
+sex = input ("Are you a boy or a girl?:")
+favC = input ("What is your favourite colour?:")
+
+
+print ("There was once a " + sex + " named " + name + " " + lname + " and their colour of choice is " + favC + "!")
